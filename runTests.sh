1b82c9d138c135986bed4e793e37cc3968178383
Roman Ondráček <ondracek.roman@centrum.cz>
2018-11-02 23:43:28 +0100

    Add script test running
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

---
+++ runTests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for d in test/*/ ; do
+    echo "$d starts"
+    ./bin/proj1 ${d}/cmd < ${d}/test.in > ${d}/test.output
+    echo "$d outputs $?"
+    diff --color ${d}/test.out ${d}/test.output
+    rm ${d}/test.output
+done
